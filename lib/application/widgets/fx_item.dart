37f6857c6360894ed5c92b4c420884a1a4f48642
Nadav Ribak <nadav.ribak@chronisense.com>
2024-02-20 15:05:48 +0200

    Navigation #2

--- lib/application/widgets/fx_item.dart
+++ lib/application/widgets/fx_item.dart
@@ -1,25 +1,31 @@
-import 'package:currencies/di/injector.dart';
+import 'package:currencies/application/routes/app_route.dart';
+import 'package:currencies/application/routes/app_route_mixin.dart';
 import 'package:currencies/domain/currency.dart';
 import 'package:flutter/material.dart';
 
-class FXItem extends StatelessWidget {
+class FXItem extends StatelessWidget with AppRouteMixin {
   final Currency currency;
 
   const FXItem({super.key, required this.currency});
 
-  void _handleTap() {
-    //TODO: temp! remove later
-    final repo = Resolver.instance().currenciesRepositories;
-    repo.getCurrenciesBy(currency.key)
-        .then((list) {
-          print(list.map((el) => "${el.timestamp}: ${el.exchange}"));
-    });
+  void _handleTap(BuildContext context) {
+    navigate(context, HistoryRoute(currencyKey: currency.key));
+    // Navigator
+    //     .pushNamed(
+    //     context, "/history",
+    //     arguments: currency.key
+    // );
+    // Navigator.of(context).push(
+    //   MaterialPageRoute(
+    //       builder: (context) => HistoryScreen(currencyKey: currency.key)
+    //   )
+    // );
   }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: () => _handleTap(),
+      onTap: () => _handleTap(context),
         title: Text(currency.name),
         subtitle: Text(currency.exchange.toStringAsFixed(3)),
         leading: Text(currency.flag)
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:currencies/application/routes/app_route.dart';
 import 'package:currencies/application/widgets/app.dart';
 import 'package:flutter/material.dart';
 
@@ -5,9 +6,19 @@ import 'di/injector.dart';
 
 void main() {
   configureDependencies();
+
   runApp(
     MaterialApp(
-      home: App(),
+      initialRoute: HomeRoute().name,
+      onGenerateRoute: (setting) {
+        final route = setting.arguments;
+        if(route is AppRoute) {
+          final builder = route.builder;
+          return MaterialPageRoute(builder: builder);
+        } else {
+          return MaterialPageRoute(builder: (context) => App());
+        }
+      },
     )
   );
 }
